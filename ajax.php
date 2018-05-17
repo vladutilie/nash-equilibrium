ad3a11dbc4df91916d7b9f11db909054bc67560f
Vlăduț Ilie <vladilie94@gmail.com>
2018-05-17 17:44:02 +0300

    Add files via upload

--- ajax.php
+++ ajax.php
@@ -38,33 +38,36 @@ if ( 'generate_matrix_form' == $form_data['action'] ) {
 } else if ( 'calculate_p' == $form_data['action'] ) {
 	$n = (int) $form_data['N'];	// number of doors
 	$k = (int) $form_data['K'];	// number of loops
+	if ( $n <=  0 || $k <= 0 ) {
+		$response['HTML'] = 'N or K cannot be <= 0';
+	} else {	
+		$S1_pass = $S2_pass = 0;
+		for ( $j = 0; $j < $k; $j++ ) {
+			init_doors( $doors, $n );
+			strategy_1( $doors, $n, $S1_pass );
+			strategy_2( $doors, $n, $S2_pass );
+		}
+		$P_S1 = probability( 'S1', $k, $S1_pass );
+		$P_S2 = probability( 'S2', $k, $S2_pass );
 
-	$S1_pass = $S2_pass = 0;
-	for ( $j = 0; $j < $k; $j++ ) {
-		init_doors( $doors, $n );
-		strategy_1( $doors, $n, $S1_pass );
-		strategy_2( $doors, $n, $S2_pass );
+		$response['HTML'] = '<table class="table table-striped table-bordered table-hover table-condensed">
+			<tr>
+				<th>Probability</th>
+				<th>1<sup>st</sup> strategy</th>
+				<th>2<sup>nd</sup> strategy</th>
+			</tr>
+			<tr>
+				<td>Pass</td>
+				<td>' . $P_S1[0] . '%</td>
+				<td>' . $P_S2[0] . '%</td>
+			</tr>
+			<tr>
+				<td>Fail</td>
+				<td>' . $P_S1[1] . '%</td>
+				<td>' . $P_S2[1] . '%</td>
+			</tr>
+		</table>';
 	}
-	$P_S1 = probability( 'S1', $k, $S1_pass );
-	$P_S2 = probability( 'S2', $k, $S2_pass );
-
-	$response['HTML'] = '<table class="table table-striped table-bordered table-hover table-condensed">
-		<tr>
-			<th>Probability</th>
-			<th>1<sup>st</sup> strategy</th>
-			<th>2<sup>nd</sup> strategy</th>
-		</tr>
-		<tr>
-			<td>Pass</td>
-			<td>' . $P_S1[0] . '%</td>
-			<td>' . $P_S2[0] . '%</td>
-		</tr>
-		<tr>
-			<td>Fail</td>
-			<td>' . $P_S1[1] . '%</td>
-			<td>' . $P_S2[1] . '%</td>
-		</tr>
-	</table>';
-}else { $response = 0; }
+} else { $response = 0; }
 
 echo json_encode( $response );
